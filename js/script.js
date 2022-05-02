829d391bc08cf1ef4a6de016e8a1256834e04b9f
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-02 17:05:51 +0000

    7 files

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 // Copyright (c) 2022 Marshall All rights reserved
 //
 // Created by: Marshall
-// Created on: Apr 2022
+// Created on: May 2022
 // This file contains the JS functions for index.html
 
+'use strict'
+
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Unit5-04-JS/sw.js", {
+    scope: "/ICS20-Unit5-04-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function calculates if you get free admission
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  // input
+  const sunday = document.getElementById("sunday").checked
+  const monday = document.getElementById("monday").checked
+  const tuesday = document.getElementById("tuesday").checked
+  const wednesday = document.getElementById("wednesday").checked
+  const thursday = document.getElementById("thursday").checked
+  const friday = document.getElementById("friday").checked
+  const saturday = document.getElementById("saturday").checked
+  var age = document.getElementById("age").value
+  
+  // process and output
+  if (tuesday == true || thursday == true || age > 12 && age < 21) {
+    document.getElementById("museum").innerHTML = 'You can get free admission at the museum!'
+  } else {
+    document.getElementById("museum").innerHTML = 'You have to pay admission at the museum!'
+  }
 }
